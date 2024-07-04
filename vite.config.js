9d4d637b5a5a4c8a97c685f3c16b6f406d83e73a
Julian Peruzzi <peru964@gmail.com>
2024-07-03 23:59:04 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://newsapi.org',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
