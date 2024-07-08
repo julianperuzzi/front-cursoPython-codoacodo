35d7d998c3b4937a96205e7a7e8ba6495090002f
Julian Peruzzi <peru964@gmail.com>
2024-07-07 22:22:56 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ function Navbar({ darkMode, toggleDarkMode }) {
       </div>
       {isOpen && (
         <div className="md:hidden fixed inset-0 bg-gray-900 bg-opacity-75 z-40 flex justify-end">
-          <div className="w-2/3 bg-white shadow-lg flex flex-col p-4">
+          <div className={`w-2/3 bg-white shadow-lg flex flex-col p-4 ${darkMode ? 'bg-slate-800 text-gray-200' : 'text-gray-300'}`}>
             <button onClick={toggleMenu} className="self-end mb-4">
               <svg className="w-6 h-6 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
@@ -84,7 +84,7 @@ function Navbar({ darkMode, toggleDarkMode }) {
             {isAuthenticated ? (
               <button onClick={logout} className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-200">Logout</button>
             ) : (
-              <Link to="/login" onClick={closeMenu} className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Login</Link>
+              <Link to="/login" onClick={closeMenu} className={`block px-4 py-2 ${darkMode ? 'text-gray-300' : 'text-gray-800'} hover:bg-gray-200`}>Login</Link>
             )}
             <button
               onClick={() => {
